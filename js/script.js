85598f39ff4a74d1a2135ac1ca2811f364c8359e
Ryan-Duffett <124389615+Ryan-Duffett@users.noreply.github.com>
2023-03-23 17:11:26 +0000

    did the correct work

--- js/script.js
+++ js/script.js
@@ -8,9 +8,6 @@
  * Do basic math.
  */
 function doMathClicked() {
-  document.getElementById("add-math").innerHTML = "<p>6 + 5 = " + (6 + 5) + "</p>"
-  document.getElementById("subtract-math").innerHTML = "<p>7 - 3 =  " + (7 - 3) + "</p>"  
-  document.getElementById("multiply-math").innerHTML = "<p>3 + 4 × 2 = " + (3 + 4 * 2) + "</p>" 
-  document.getElementById("divide-math").innerHTML = "<p>(4 ÷ 2) + 3 = " + (4 / 2 + 3) + "</p>"
-  document.getElementById("exponent-math").innerHTML = "<p>5 + 2³ = " + (5 + 2 ** 3) + "</p>"
+  document.getElementById("area").innerHTML = "<p>Area = " + (5 * 3) + "cm² </p>"
+  document.getElementById("perimeter").innerHTML = "<p>Perimeter = " + (5 + 5 + 3 + 3) + "cm </p>"  
 }
